4dd5814e264f3eb3666b598db5938c92aecee7af
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-03-07 23:17:46 -0300

    requisito 9

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,4 +1,4 @@
-const { createCategory } = require('../services/category.service');
+const { createCategory, getAllCategories } = require('../services/category.service');
 
 const createCategoryController = async (req, res) => {
   try {
@@ -10,4 +10,13 @@ const createCategoryController = async (req, res) => {
   }
 };
 
-module.exports = { createCategoryController };
+const getAllCategoriesController = async (req, res) => {
+  try {
+    const categories = await getAllCategories();
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createCategoryController, getAllCategoriesController };
--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const { createCategoryController } = require('../controller/category.controller');
+const {
+  createCategoryController,
+  getAllCategoriesController,
+} = require('../controller/category.controller');
 const { checkToken } = require('../middleware/checkToken');
 const { checkCategoryName } = require('../middleware/validateCategoryName');
 
 const categoryRoute = express.Router();
 
-categoryRoute.post('/', checkToken, checkCategoryName, createCategoryController);
+categoryRoute.post(
+  '/',
+  checkToken,
+  checkCategoryName,
+  createCategoryController,
+);
+categoryRoute.get('/', checkToken, getAllCategoriesController);
 
 module.exports = categoryRoute;
