858ce99354536328a1a9a6f9d94cb56e56c87b31
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-03-07 21:51:49 -0300

    requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  }
+  );
+  return Category;
+}
